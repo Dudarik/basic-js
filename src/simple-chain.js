2bbeb9f12d1d961b6b8d2bc7b56d84d2f694f4f7
Dudarik <Dudarik@yandex.ru>
2022-10-11 19:54:56 +0500

    feat: chain is ready

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,70 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
-const chainMaker = {
+ const chainMaker = {
+  store: [],
+  toString(value) {
+    if (arguments.length === 0) return "( )";
+    switch (value) {
+      case null:
+        return "( null )";
+      case undefined:
+        return "( undefined )";
+      case Infinity:
+        return "( Infinity )";
+      case false:
+        return "( false )";
+
+      default:
+        return `( ${String(value)} )`;
+    }
+  },
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    return this.store.length
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    // console.log(value);
+
+    this.store.push(this.toString(value));
+    return this;
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    try {
+      // debugger;
+      if (!(position - 1 in this.store))
+        throw new Error("You can't remove incorrect link!");
+    } catch (error) {
+      this.store = [];
+      throw error;
+    }
+
+    this.store.splice(position - 1, 1);
+    return this;
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    this.store.reverse();
+    return this;
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    const retStore = this.store.slice();
+    this.store = [];
+    return retStore.join("~~");
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-  }
+  },
 };
-
 module.exports = {
-  chainMaker
+  chainMaker,
 };
