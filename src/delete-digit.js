94dd3fd0952697a5602fced78d1ddd05605c9aad
Dudarik <Dudarik@yandex.ru>
2022-10-12 22:34:12 +0500

    feat: delete-digit, bad code, coz i want to sleep

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const tArr = [...String(n)];
+  const tObj = {};
+  let max = -1;
+
+  for (let i = 0; i < tArr.length; i++) {
+    tObj[tArr[i]] ? tObj[tArr[i]].push(i) : (tObj[tArr[i]] = [i]);
+  }
+
+  const tArr2 = Array.from(Object.entries(tObj));
+  for (let i = 0; i < tArr2.length; i++) {
+    const cp = tArr.slice();
+    cp.splice(tArr2[i][1][0], 1);
+    max = Math.max(max, +cp.join(""));
+  }
+  return max;
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
