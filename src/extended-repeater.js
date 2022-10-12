4e0dd9956390ec24bdea8a4ea0346f2651b144db
Dudarik <Dudarik@yandex.ru>
2022-10-12 15:44:55 +0500

    feat: extended-repeater ready to repeat

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,62 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(
+  str,
+  {
+    repeatTimes = 0,
+    separator = "+",
+    addition = "",
+    additionRepeatTimes = 0,
+    additionSeparator = "|",
+  } = {}
+) {
+  const createAddition = (addition, additionRepeatTimes, additionSeparator) => {
+    // console.log(addition, String(additionRepeatTimes));
+    if (addition && additionRepeatTimes) {
+      const addArray = [];
+      for (let k = 0; k < additionRepeatTimes; k++) {
+        addArray.push(addition);
+      }
+      return addArray.join(additionSeparator);
+    }
+    return "";
+  };
+
+  if (!repeatTimes && !additionRepeatTimes && addition)
+    return str + createAddition(String(addition), 1, additionSeparator);
+
+  if (addition && repeatTimes && !additionRepeatTimes) additionRepeatTimes = 1;
+
+  // debugger;
+  if (!repeatTimes) return str;
+  const result = [];
+  let add = createAddition(
+    String(addition),
+    additionRepeatTimes,
+    additionSeparator
+  );
+
+  for (let i = 0; i < repeatTimes; i++) {
+    result.push(str + add);
+  }
+  return result.join(separator);
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
