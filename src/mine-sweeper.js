64dac92a87d572692fea537e6c6037605732ffed
Dudarik <Dudarik@yandex.ru>
2022-10-14 14:28:01 +0500

    feat: mine-sweeper ready to work

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  // throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
+  // const neighbors = [
+  //   [r - 1, c],
+  //   [r - 1, c + 1],
+  //   [r, c + 1],
+  //   [r + 1, c + 1],
+  //   [r + 1, c],
+  //   [r + 1, c - 1],
+  //   [r, c - 1],
+  //   [r - 1, c - 1],
+  // ];
+  const nghRow = [-1, -1, 0, 1, 1, 1, 0, -1];
+  const nghCol = [0, 1, 1, 1, 0, -1, -1, -1];
+
+  const isInMatrix = (row, col, matRow, matCol) => {
+    // console.log(row, col);
+    if (row < 0 || col < 0 || row > matRow || col > matCol) return false;
+    return true;
+  };
+
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    const row = [];
+    for (let k = 0; k < matrix[0].length; k++) {
+      let countMines = 0;
+      for (let n = 0; n < nghRow.length; n++) {
+        if (
+          isInMatrix(
+            i + nghRow[n],
+            k + nghCol[n],
+            matrix.length - 1,
+            matrix[0].length - 1
+          )
+        ) {
+          // console.log(matrix[i + nghRow[n]][k + nghCol[n]]);
+          if (matrix[i + nghRow[n]][k + nghCol[n]] === true) countMines += 1;
+
+          // console.log(countMines);
+        }
+      }
+      row.push(countMines);
+    }
+    result.push(row);
+  }
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
