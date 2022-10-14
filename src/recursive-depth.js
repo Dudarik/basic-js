f7f4501b7e59d57c8e3001a53bf8a59e03587ce4
Dudarik <Dudarik@yandex.ru>
2022-10-14 10:45:50 +0500

    fix: minor changes

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -32,7 +32,6 @@ class DepthCalculator {
 
     for (let i = 0; i < arr.length; i++) {
       if (Array.isArray(arr[i])) {
-        depth = 1;
         arr = arr.flat();
         return (depth += this.calculateDepth(arr));
       }
