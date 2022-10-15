d7485b8ed50d296809d4d1cb2278fc75af2f6b52
Dudarik <Dudarik@yandex.ru>
2022-10-15 15:17:51 +0500

    feat: vigenere-chipher ready to work

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,103 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    const FIRST_LETTER_CODE = 65;
+    const COUNT_ALPHABET_LETTERS = 26;
+
+    this.direct = direct;
+
+    this.alphBig = Array(COUNT_ALPHABET_LETTERS)
+      .fill(0)
+      .map((_, index) => String.fromCharCode(FIRST_LETTER_CODE + index));
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  _getKey = (str, key) => {
+    let keyId = 0;
+    return [...str]
+      .map((char) => {
+        if (!this.alphBig.includes(char)) return " ";
+        if (keyId === key.length) keyId = 0;
+        return key[keyId++];
+      })
+      .join("");
+  };
+
+  _getCodeFromAlph = (char) => this.alphBig.findIndex((item) => item === char);
+
+  _charToCode = (char, keyChar) => {
+    const charCode = this._getCodeFromAlph(char);
+    const keyCharCode = this._getCodeFromAlph(keyChar);
+
+    if (charCode === -1) return char;
+
+    return this.alphBig[(charCode + keyCharCode) % this.alphBig.length];
+  };
+
+  _codeToChar = (char, keyChar) => {
+    const charCode = this._getCodeFromAlph(char);
+    const keyCharCode = this._getCodeFromAlph(keyChar);
+
+    if (charCode === -1) return char;
+
+    const charPosInAlph = (charCode - keyCharCode) % this.alphBig.length;
+
+    return charPosInAlph >= 0
+      ? this.alphBig[charPosInAlph]
+      : this.alphBig[this.alphBig.length + charPosInAlph];
+  };
+
+  encrypt(...args) {
+    if (args.length < 2 || !args[0] || !args[1])
+      throw new Error("Incorrect arguments!");
+
+    let message = args[0].toUpperCase(),
+      key = args[1].toUpperCase();
+
+    if (message.length > key.length) key = this._getKey(message, key);
+
+    const result = [...message].map((char, index) =>
+      this._charToCode(char, key[index])
+    );
+
+    return this.direct ? result.join("") : result.reverse().join("");
+  }
+
+  decrypt(...args) {
+    if (args.length < 2 || !args[0] || !args[1])
+      throw new Error("Incorrect arguments!");
+
+    let message = args[0].toUpperCase(),
+      key = args[1].toUpperCase();
+    if (message.length > key.length) key = this._getKey(message, key);
+
+    const result = [...message].map((char, index) =>
+      this._codeToChar(char, key[index])
+    );
+
+    return this.direct ? result.join("") : result.reverse().join("");
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
